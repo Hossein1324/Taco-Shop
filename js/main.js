db75a3c28e4a7dd3339bf288677e8306f514fde9
Hossein <hosseinpro@gmail.com>
2024-05-15 00:44:59 +0430

    Push in github

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 const year = document.getElementById("year")
 const thisYear = new Date().getFullYear()
 year.setAttribute("datetime", thisYear)
-year.textContent = thisYear
+year.textContent = thisYear;
+
+function mood () {
+    document.getElementById('darkmood').classList.toggle('bgcolor');
+};
