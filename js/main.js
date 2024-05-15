2455bea3355122f274d614c38a6741194c0dc8db
Hossein <hosseinpro@gmail.com>
2024-05-16 01:40:36 +0430

    add price sectoin

--- js/main.js
+++ js/main.js
@@ -6,3 +6,43 @@ year.textContent = thisYear;
 function mood () {
     document.getElementById('darkmood').classList.toggle('bgcolor');
 };
+let price = 0;
+
+function  yourprice1() {
+   const g = document.querySelector('.yourprice');
+   price = price+1.50;
+    g.innerHTML = `${price} $`
+}
+
+
+function  yourprice2() {
+    const g = document.querySelector('.yourprice');
+    price = price+2.50;
+     g.innerHTML = `${price} $`
+ }
+
+ function  yourprice3() {
+    const g = document.querySelector('.yourprice');
+    price = price+3.25;
+     g.innerHTML = `${price} $`
+ }
+
+ 
+function  yourprice1s() {
+    const g = document.querySelector('.yourprice');
+    price = price+2;
+     g.innerHTML = `${price} $`
+ }
+ 
+ function  yourprice2s() {
+     const g = document.querySelector('.yourprice');
+     price = price+3.50;
+      g.innerHTML = `${price} $`
+  }
+ 
+  function  yourprice3s() {
+     const g = document.querySelector('.yourprice');
+     price = price+4.50;
+      g.innerHTML = `${price} $`
+  }
+     
